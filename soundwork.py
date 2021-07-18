8aaba35021edbd082554ac60f6f50dc0ef76fcb2
EightNice <jtoril8@gmail.com>
2021-07-18 19:10:33 +0200

    Added some sounds

---
+++ soundwork.py
@@ -0,0 +1,63 @@
+from colorama import Fore, Style, init
+import progressbar
+import time
+import subprocess
+import signal
+
+yw = Fore.YELLOW
+gr = Fore.GREEN
+bl = Fore.BLUE
+sr = Style.RESET_ALL
+sb = Style.BRIGHT
+
+init()
+
+def bMiner_nosound():
+    time.sleep(1)
+    print(" \n" + yw + "Connecting to www.minershop.org... Connected." + sr)
+    time.sleep(2)
+    print(gr + " HTTP request sent, awaiting response... 200 OK")
+    time.sleep(1)
+    print("Lenght: 10701 (10k) [text/html]\n Saving to: 'index.html'" +sr + sb)
+    time.sleep(1)
+    for i in progressbar.progressbar(range(100)): time.sleep(0.03)
+    time.sleep(1)
+    print(sr + gr + "Saved to 'index.html'\n" + sr)
+    time.sleep(1)
+    print(sb + bl + "[-=-=-Miner Shop-=-=-]" + sr)
+
+def bMinerSound():
+    time.sleep(1)
+    print(" \n" + yw + "Connecting to www.minershop.org... Connected." + sr)
+    time.sleep(2)
+    print(gr + " HTTP request sent, awaiting response... 200 OK")
+    time.sleep(1)
+    print("Lenght: 10701 (10k) [text/html]\n Saving to: 'index.html'" +sr + sb)
+    time.sleep(1)
+    p = subprocess.Popen('play-audio ' + './sounds/connect1.mp3 ./sounds/connect2.mp3', shell=True)
+    for i in progressbar.progressbar(range(100)): time.sleep(0.03)
+    time.sleep(1)
+    print(sr + gr + "Saved to 'index.html'\n" + sr)
+    time.sleep(1)
+    print(sb + bl + "[-=-=-Miner Shop-=-=-]" + sr)
+
+def bankSound():
+    print(" \n" + yw + "Connecting to your Darknet Bank account..." + sr + sb)
+    time.sleep(1)
+    p = subprocess.Popen('play-audio ' + './sounds/connect1.mp3', shell=True)
+    p = subprocess.Popen('play-audio ' + './sounds/connect2.mp3', shell=True)
+    time.sleep(0.4)
+    for i in progressbar.progressbar(range(100)): time.sleep(0.015)
+    time.sleep(1)
+    print(sr + gr + "Connected!\n" + sr)
+    time.sleep(1)
+    print(yw + "Welcome to DarkNet Bank!\n" + sr)
+
+def bankNosound():
+    print(" \n" + yw + "Connecting to your Darknet Bank account..." + sr + sb)
+    time.sleep(1)
+    for i in progressbar.progressbar(range(100)): time.sleep(0.015)
+    time.sleep(1)
+    print(sr + gr + "Connected!\n" + sr)
+    time.sleep(1)
+    print(yw + "Welcome to DarkNet Bank!\n" + sr)
